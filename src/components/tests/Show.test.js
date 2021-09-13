09c39b6dc2cadcaa833f21421ccba921254a0f36
Allison Homem <mrs.allisonhomem@outlook.com>
2021-09-13 15:38:24 -0500

    all tests running and passing for Show component

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -5,22 +5,70 @@ import userEvent from '@testing-library/user-event';
 import Show from './../Show';
 
 const testShow = {
-    //add in approprate test data structure here.
+    name: "",
+    summary: "",
+    seasons: [{
+        id: 1,
+        name: "Rise of the Demo-dogs",
+        episodes: [],
+    },
+    {
+        id: 2,
+        name: "Return of the Nerdy Teens",
+        episodes: [],
+    },
+    {
+        id: 3,
+        name: "The Legend of Hawkins",
+        episodes: [],
+    }],
+
 }
 
 test('renders testShow and no selected Season without errors', ()=>{
+    render(<Show show={testShow} selectedSeason={"none"}/>)
 });
 
 test('renders Loading component when prop show is null', () => {
+    render(<Show show={null} />);
+
+    const loading = screen.queryByTestId('loading-container');
+
+    expect(loading).toBeInTheDocument();
 });
 
 test('renders same number of options seasons are passed in', ()=>{
+    render(<Show show={testShow} selectedSeason={"none"}/>);
+
+    const seasonOptions = screen.queryAllByTestId('season-option');
+
+    expect(seasonOptions).toHaveLength(3);
 });
 
 test('handleSelect is called when an season is selected', () => {
+    const handleSelect = jest.fn();
+    
+    render(<Show show={testShow} selectedSeason={"none"} handleSelect={handleSelect}/>);
+
+    const selected = screen.getByLabelText(/select a season/i);
+
+    userEvent.selectOptions(selected, ['2']);
+
+    expect(handleSelect).toBeCalled();
 });
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+    render(<Show show={testShow} selectedSeason={"none"}/>);
+
+    let episodes = screen.queryByTestId('episodes-container');
+
+    expect(episodes).not.toBeInTheDocument();
+
+    render(<Show show={testShow} selectedSeason={1}/>);
+
+    episodes = screen.queryByTestId('episodes-container');
+
+    expect(episodes).toBeInTheDocument();
 });
 
 //Tasks:
